4ee979efa67eba7bf03b7808c43ec03109e082d9
pkabc <shanghongfei@kylinos.cn>
2020-11-19 17:01:02 +0800

    add comment

--- main.cpp
+++ main.cpp
@@ -4,5 +4,5 @@
 int main(int, char**) {
   int aa[] = {1, 3, 45, 10, 34, 30, 4, 8, 33};
   sort_bubble(aa, sizeof(aa) / sizeof(aa[0]));
-  std::cout << "Hello, world!\n";
+  std::cout << "Hello, world! dev branch\n";
 }
