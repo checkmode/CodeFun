8c3f7e9256dd4f7a8de59ecf054886bb6b7fbd10
pkabc <shanghongfei@kylinos.cn>
2020-11-19 20:51:22 +0800

    rebase master to dev

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int main(int, char**) {
   int aa[] = {1, 3, 45, 10, 34, 30, 4, 8, 33};
   sort_bubble(aa, sizeof(aa) / sizeof(aa[0]));
   std::cout << "Hello, world! dev branch, also change\n";
-  std::cout << "change from master again\n";
+  std::cout << "change from master again, dev update\n";
   return 0;
 }
 
