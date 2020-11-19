a5dd2eaee81507afe465669c8e76dfaa574b4f01
pkabc <shanghongfei@kylinos.cn>
2020-11-19 20:48:34 +0800

    add print master function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include "sortfunctions.h"
 
+void printfMaster()
+{
+	printf("printf master");
+}
+
 int main(int, char**) {
   int aa[] = {1, 3, 45, 10, 34, 30, 4, 8, 33};
   sort_bubble(aa, sizeof(aa) / sizeof(aa[0]));
@@ -8,3 +13,5 @@ int main(int, char**) {
   std::cout << "change from master again\n";
   return 0;
 }
+
+
